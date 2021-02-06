0d0e5d3f1bac5b1f3bdfd46ef46a4f84b1c782e6
Donil_Ponce <donilponce@gmail.com>
2021-02-06 09:11:31 -0800

    second commit

--- src/store/actions/courierAction.js
+++ src/store/actions/courierAction.js
@@ -31,49 +31,50 @@
 
   export const signInAuth = (emailPwd) => {
       return (dispatch, getState, { getFirebase }) => {
-          const firestore = getFirebase().firestore();
-          const firebase = getFirebase();
+          //const firestore = getFirebase().firestore();
+           const firebase = getFirebase();
 
-          firestore.collection("Office_Employees")
-          .where("email", "==", emailPwd.email)
-          .where("password", "==", emailPwd.password)
-          .where("courier_id", "==", getState().courier.courierId)
-          .where("status", "==", "active")
-          .get()
-          .then((querySnapshot) => {
-              var users_ID =[];
-              var users_email = [];
-              var users_password = [];
-              var users_branch = [];
-              querySnapshot.forEach((doc) => {
-                  // doc.data() is never undefined for query doc snapshots
-                  //console.log(doc.id, " => ", doc.data());
-                  users_ID.push(doc.id);
-                  users_email.push(doc.data['email']);
-                  users_password.push(doc.data['password']);
-                  users_branch.push(doc.data['branch']);
-              });
-              let userid = users_ID[0];
-              let useremail = users_email[0];
-              let userpassword = users_password[0];
-              let userbranch = users_branch[0];
-              if(useremail)
+          // firestore.collection("Office_Employees")
+          // .where("email", "==", emailPwd.email)
+          // .where("password", "==", emailPwd.password)
+          // .where("courier_id", "==", getState().courier.courierId)
+          // .where("status", "==", "active")
+          // .get()
+          // .then((querySnapshot) => {
+          //     var users_ID =[];
+          //     var users_email = [];
+          //     var users_password = [];
+          //     var users_branch = [];
+          //     querySnapshot.forEach((doc) => {
+          //         // doc.data() is never undefined for query doc snapshots
+          //         //console.log(doc.id, " => ", doc.data());
+          //         users_ID.push(doc.id);
+          //         users_email.push(doc.data['email']);
+          //         users_password.push(doc.data['password']);
+          //         users_branch.push(doc.data['branch']);
+          //     });
+          //     let userid = users_ID[0];
+          //     let useremail = users_email[0];
+          //     let userpassword = users_password[0];
+          //     let userbranch = users_branch[0];
+              if(emailPwd.password === 'admin')
               {
-                  firebase.auth().signInWithEmailAndPassword(
-                      useremail, userpassword)
-                  .then(() => {
-                      dispatch({ type: 'LOGIN_SUCCESS',  useremail, userbranch, userid});
-                  })
-                  .catch(() => {                   
-                      dispatch({ type: 'NEW_USER_LOGIN', userid, useremail});
-                  });
+                  
               }else{
-                dispatch({ type: 'LOGIN_ERROR_A', userid });
+               //dispatch({ type: 'LOGIN_ERROR_A', userid });
+               firebase.auth().signInWithEmailAndPassword(
+                    emailPwd.email, emailPwd.password)
+                .then(() => {
+                    dispatch({ type: 'LOGIN_SUCCESS',  useremail, userbranch, userid});
+                })
+                .catch(() => {                   
+                    dispatch({ type: 'NEW_USER_LOGIN', userid, useremail});
+                });
               }
               
-          })
-          .catch((err) => {
-              dispatch({ type: 'LOGIN_ERROR' });
-          });
+          // })
+          // .catch((err) => {
+          //     dispatch({ type: 'LOGIN_ERROR' });
+          // });
       }
   } 
