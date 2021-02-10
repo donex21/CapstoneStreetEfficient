76b9f131e03cfea6b39f146a9cb9a5e65499743c
Donil_Ponce <donilponce@gmail.com>
2021-02-09 16:04:32 -0800

    my fourth commit

--- src/components/AddOfficeEmployee.js
+++ src/components/AddOfficeEmployee.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import '../styles/AddOfficeEmployee.scss'
 
-export const AddOfficeEmployee = () => {
+export const AddOfficeEmployee = (props) => {
+    const {show, close} = props
     return (
-        <div>
+        <div className="modal-wrapper"
+            style={{
+                transform: show ? 'translateY(0vh)' : 'translateY(-100vh)',
+                opacity: show ? '1' : '0'
+            }}
+        >
+            <div className="modal-header">
+                            <h2>Add Office Employee</h2>
+                            <span onClick={close} className="close-modal-btn"> x </span>
+            </div>
             
         </div>
     )
